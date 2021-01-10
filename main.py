157f892280a9e6023d0a50fb3864b8330379c799
alithethird <39213991+alithethird@users.noreply.github.com>
2021-01-10 20:39:51 +0300

    motor driver functionized

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ class MyTableWidget(QWidget):
         self.timer.setInterval(50)
         self.timer.timeout.connect(self.update_plot)
         self.timer.start()
-        md = motor_driver()
+        md = motor_driver.motor_driver()
         md.motor_run(0.001, 400, 1)
 
     def stop_test(self):
