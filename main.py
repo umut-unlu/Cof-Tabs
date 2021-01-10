fb166be745eef23778260dad8ba94092a77244fc
alithethird <39213991+alithethird@users.noreply.github.com>
2021-01-10 21:08:37 +0300

    motor driver demo

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import pyqtgraph as pg
 
 import RPi.GPIO as GPIO
 from hx711 import HX711
-#import motor_driver
+import motor_driver
 
 # set up the load cell
 hx = HX711(5, 6)
@@ -84,7 +84,7 @@ class MyTableWidget(QWidget):
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
         pen = pg.mkPen(color=(255, 0, 0))
-        self.data_line = self.graphicsView.plot(self.x, self.y, pen=pen)
+        self.data_line = self.graphicsView.plot(self.test_time, self.test_data, pen=pen)
         # button events
         self.pushButtonStart.clicked.connect(self.start_test) # plot when clicked
         self.pushButtonStop.clicked.connect(self.stop_test) # tare when clicked
@@ -97,8 +97,8 @@ class MyTableWidget(QWidget):
         self.timer.setInterval(50)
         self.timer.timeout.connect(self.update_plot)
         self.timer.start()
-        #md = motor_driver.motor_driver()
-        #md.motor_run(0.001, 400, 1)
+        md = motor_driver.motor_driver()
+        md.motor_run(0.001, 400, 1)
 
     def stop_test(self):
         self.timer.stop()
