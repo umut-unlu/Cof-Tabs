62234e837b708bf97981dcb8fa6da82c6f422ef3
alithethird <39213991+alithethird@users.noreply.github.com>
2021-01-12 14:37:38 +0300

    results tab

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ class MyTableWidget(QWidget):
 
         # Create third tab
         self.tab3.layout = QVBoxLayout(self)
-        self.cof = QtWidgets.Qlabel('test results')
+        self.cof = QtWidgets.QlineEdit('test results')
 
         # Set Plotter
         pg.setConfigOption('background', 'w')
